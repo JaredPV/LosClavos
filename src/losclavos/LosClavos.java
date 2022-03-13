4e448f04a7bc133824b04948cc2ee9102a97ec05
JaredPV <jaredpv99@micorreo.upp.edu.mx>
2022-03-13 01:44:19 -0600

    inicioSesion y Conexion

--- src/losclavos/LosClavos.java
+++ src/losclavos/LosClavos.java
@@ -5,6 +5,8 @@
  */
 package losclavos;
 
+import modelo.Conexion;
+
 /**
  *
  * @author jared
@@ -15,7 +17,8 @@ public class LosClavos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Conexion con = new Conexion();
+        con.getConexion();
     }
     
 }
--- src/modelo/MDB.java
+++ src/modelo/MDB.java
@@ -5,10 +5,36 @@
  */
 package modelo;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  *
  * @author jared
  */
-public class MDB {
-    
+public class MDB extends Conexion {
+
+    public boolean inicioSesion (Usuario u){
+        PreparedStatement ps = null;
+        Connection con = getConexion();
+        ResultSet rs = null;
+        String sql = "SELECT * FROM usuario WHERE numeroEmpleado=? AND pass=? LIMIT 1";
+        try{
+            ps = con.prepareCall(sql);
+            ps.setInt(1, u.getNumeroEmpleado());
+            ps.setString(2, u.getPass());
+            rs = ps.executeQuery();
+            if (rs.next()){
+                u.setNumeroEmpleado(Integer.parseInt(rs.getString("numeroEmpleado")));
+                u.setPass(rs.getString("pass"));
+                return true;
+            }
+            return false;
+        }catch(SQLException e){
+            System.err.println(e);
+            return false;
+        }
+    }
 }
